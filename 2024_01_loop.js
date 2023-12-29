899928c63f7378e69d239ba09b9f05145f0585fb
Bhawani_K <bk.srivaishu@outlook.com>
2023-12-29 12:22:39 +0530

    basic Loop

--- 2024_01_loop.js
+++ 2024_01_loop.js
@@ -1,3 +1,6 @@
+//= basic working of loop,
+//= it should initialize, condition, increment or decrement or any condition as such the loop should exits.
+
 //. for Loop
 // let arr=[1,2,3,4]
 // for(let i=0; i<arr.length;i++){
@@ -26,19 +29,45 @@
 
 // break & continue
 for (let i = 1; i <= 20; i++) {
-    if(i ==5){
-        console.log(`Detected the value ${i}`);
-    break;  // break, breaks the loop and stops the execution after it and directly goes out of loop.
-    }
-    console.log(`value of i is ${i}`);
+  if (i == 5) {
+    console.log(`Detected the value ${i}`);
+    break; // break, breaks the loop and stops the execution after it and directly goes out of loop.
+  }
+  console.log(`value of i is ${i}`);
 }
 // ---------------------------------------------------
 for (let i = 1; i <= 20; i++) {
-    if(i ==5){
-        console.log(`Detected the value ${i}`);
-    continue;   // skips the flow for one time and continues with the next execution
-    }
-    console.log(`value of i is ${i}`);
+  if (i == 5) {
+    console.log(`Detected the value ${i}`);
+    continue; // skips the flow for one time and continues with the next execution
+  }
+  console.log(`value of i is ${i}`);
 }
 
+//. while loop
+let i = 0;
+while (i <= 10) {
+  console.log(`value of index is ${i}`);
+  i = i + 2;
+}
 
+let myArr = [1, 2, 3, 4, 5, 6, 7, 8];
+let arr = 0; //initialization
+while (arr < myArr.length) {
+  // condition
+  console.log(`value is ${myArr[arr]}`);
+  arr++; // condition to exit the loop else it would print the 1st value infinite times.
+}
+//. do-while loop
+let score = 1; // initialization
+do {
+  console.log(`Score is ${score}`);
+  score++; // condition to exit the loop
+} while (score <= 10); // condition
+
+// ---------------------------------------------------------
+let winScore = 11; // initialization
+do {
+  console.log(`winScore is ${winScore}`);   //prints11, as it does the job 1st and checks the condition later.
+  score++; // condition to exit the loop
+} while (winScore <= 10); // condition

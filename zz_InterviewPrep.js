59481e2ee9302d96e8366d6b25a41b6bd30e8702
Bhawani_K <bk.srivaishu@outlook.com>
2024-01-06 22:07:10 +0530

    nodeList

--- zz_InterviewPrep.js
+++ zz_InterviewPrep.js
@@ -75,4 +75,67 @@ const arr = [1,2,3,4]
 const val = arr.forEach((ele)=>{return ele;})
 console.log(val); //undefined
 
+//. reduce
+//= takes the callback function and an initialator
+//= reduce has 2args. (accumulator and currentVal), where it takes the result of operation as the accumulator.
+//= can be used in both Array & ArrayOfObjects.
+
+//! DOM
+//. By Id
+document.getElementById('eleId')    // returns the eltag of elements with the entered id.
+document.getElementById('eleId').id //  gets the value of the id
+document.getElementById('eleId').class // undefined
+document.getElementById('eleId').className  // gets the class with the specific eleId.
+document.getElementById('eleId').getAttribute   // gets the function
+document.getElementById('eleId').getAttribute('id') // takes up the attribute and fetches the associated value.
+document.getElementById('eleId').setAttribute('class', 'test')  // here the className value gets always overRidden.
+
+title.style.backgroundColor = 'green'
+title.style.padding = '15px'
+title.style.borderRadius = '15px'
+
+//- how to add the content
+// title.textContent 
+//> displays the whole content including the hidden which is being hidden by some css styling or by
+//> doesn't supports HTML tag
+// title.innerText , 
+//> displays only the content being visible in the document page
+//> doesn't supports HTML tag
+// title.innerHTML , 
+//> here it displays full element withe the extra tags being used.
+//> supports HTML tag
+
+//. ClassName
+document.getElementsByClassName('heading')  // returns the HTML element tag with the specific class
+
+//. querySelector
+document.querySelector('h1')  // returns 1st h1 element
+document.querySelector('#title')  // returns 1st HTML ele which matches the specific id
+document.querySelector('.heading')  // returns 1st HTML ele which matches the specific class
+document.querySelector('input[type="password"]')  // returns the HTMl ele with the specific selectors
+
+//-we can select the elements too
+const myul = document.querySelector('ul')  // gets the complete ul tag element
+const myli = myul.querySelector('li')   // As querySelector selects the 1st ele, it fetches the li element from the li list
+myli.style.backgroundColor = 'green'
+myli.style.padding = '15px'
+myli.innerText = 'qwerty' //changes the content to five.
+
+//- querySelectorAll
+const tempLiList = document.querySelectorAll('li') // gets the nodeList of all the elements with the list of extra properties. Dont confuse it with Array, Array have much more properties such as map,push pop etc...
+tempLiList.style.color = 'red'  // cannot set properties of undefined.
+tempLiList[0].style.color = 'red'  // this would change the color of the element. Please check the NodeList Properties before applying.
+
+tempLiList.forEach((ele)=>{
+    return ele.style.color = 'pink'
+})
+
+//! how to convert HTMl collection or NodeList to Array
+const convertToArray = Array.from(tempLiList) // converts an iterable object to an array.
+// now we can perform Array method operations.
+convertToArray.forEach((ele)=>{
+    return li.style.color = 'orange'
+})
+
+
 
